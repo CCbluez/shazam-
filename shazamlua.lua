7af7bb161744a4ed84700505857851482c464a79
N/ABreadsticks <91288666+CCbluez@users.noreply.github.com>
2023-03-07 18:09:05 -0500

    aaa

---
+++ shazamlua.lua
@@ -0,0 +1,71 @@
+
+
+local args = {
+    [1] = "Boombox",
+    [2] = "821439273"
+}
+
+game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
+
+wait()
+
+local args = {
+    [1] = "Shazam!",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+
+wait(0.1)
+
+local args = {
+    [1] = "wear",
+    [2] = 7116199518
+}
+
+game:GetService("ReplicatedStorage").Catalog.Body.LeftLegs.UnU:FireServer(unpack(args))
+
+wait()
+
+local args = {
+    [1] = "wear",
+    [2] = 7116201572
+}
+
+game:GetService("ReplicatedStorage").Catalog.Body.LeftLegs.UnU:FireServer(unpack(args))
+
+wait()
+
+
+local args = {
+    [1] = "wear",
+    [2] = 4602492814
+}
+
+game:GetService("ReplicatedStorage").Catalog.Body.LeftLegs.UnU:FireServer(unpack(args))
+
+wait()
+
+local args = {
+    [1] = "wear",
+    [2] = 4584983773
+    
+}
+
+game:GetService("ReplicatedStorage").Catalog.Body.LeftLegs.UnU:FireServer(unpack(args))
+
+wait(2.9)
+
+
+local args = {
+    [1] = "BoomboxStop"
+}
+
+game:GetService("ReplicatedStorage").MainEvent:FireServer(unpack(args))
+
+
+print("shazam on top")
+print("fuck icespice")
+print("fuck all yall fr")
+print("¹²³⁴⁵⁶⁷⁸⁹ im in ya car")
